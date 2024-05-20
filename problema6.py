2dd4b85543467f5ab28b6632387fa0f28e3569a4
geoy12 <145078589+geo1605@users.noreply.github.com>
2024-05-20 11:48:58 -0600

    Add files via upload

---
+++ problema6.py
@@ -0,0 +1,9 @@
+x = int(input("coloca un numero: "))
+y = int(input("coloca otro numero: "))
+
+if x==y:
+    print("son iguales")
+elif x>y: 
+    print("el primer numero es mayor")
+else:
+    print("el numero dos es mayor")
