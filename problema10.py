50704e65a8351164dafa73e3f101d61e3b289c90
geoy12 <145078589+geo1605@users.noreply.github.com>
2024-05-21 07:58:25 -0600

    programa 10 - sumas

---
+++ problema10.py
@@ -0,0 +1,10 @@
+
+while 0<1:
+    s = str(input("salir con 'n': "))
+    if s == "n":
+        break
+    n1 = float(input("numero 1: "))
+    n2 = float(input("numero 2: "))
+    r = n1 + n2
+    print("resutado", r)
+    
